cb3b678f9bb864a10b969acf1b631c9e8d962392
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-11-15 00:37:51 +0600

    Initial commit

---
+++ src/carnumberplate/CarNumberPlate.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carnumberplate;
+
+/**
+ *
+ * @author shahr
+ */
+public class CarNumberPlate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
