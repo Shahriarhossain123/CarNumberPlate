3acb5d531c277dfc8227c15cf7a89e17b5761e1e
Shahriarhossain123 <9763691+Shahriarhossain123@users.noreply.github.com>
2021-12-14 01:04:49 +0600

    home

--- src/View/Home/Home.java
+++ src/View/Home/Home.java
@@ -297,6 +297,7 @@ public class Home extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitMenuItemActionPerformed
